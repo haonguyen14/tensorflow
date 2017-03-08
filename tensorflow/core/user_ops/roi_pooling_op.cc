fe320ee4bbfcb499aa026b89514c14c9d8ea6b80
hnguyen <hao.nguyen1214@gmail.com>
2017-03-08 15:20:59 -0800

    preliminary implementation of roi pooling

--- tensorflow/core/user_ops/roi_pooling_op.cc
+++ tensorflow/core/user_ops/roi_pooling_op.cc
@@ -31,8 +31,8 @@ void RoiPoolingOp::Compute(OpKernelContext* context) {
                 errors::InvalidArgument("mismatching batch sizes"));
 
     int* output_shape = (int*) output_shape_tensor.tensor_data().data(); 
-    int output_height = output_shape[0];
-    int output_width = output_shape[1];
+    int output_h = output_shape[0];
+    int output_w = output_shape[1];
 
     int num_batches = feature_tensor.dim_size(0);
     int num_channels = feature_tensor.dim_size(3);
@@ -44,22 +44,50 @@ void RoiPoolingOp::Compute(OpKernelContext* context) {
     float* rois = (float*) roi_tensor.tensor_data().data();
 
 
-    TensorShape result_shape = {num_batches, num_rois, output_height, output_width, num_channels};
+    TensorShape result_shape = {num_batches, num_rois, output_h, output_w, num_channels};
     Tensor* output_tensor;
     OP_REQUIRES_OK(context, context->allocate_output(0, result_shape, &output_tensor));
 
+	float* output = (float*) output_tensor->tensor_data().data();
+
     for(int img_i = 0; img_i < num_batches; img_i++) {
+		float* feature_batch_start = features + (img_i * feature_h * feature_w * num_channels);
+		float* roi_batch_start = rois + (img_i * num_rois * 4);
+
         for(int channel_i = 0; channel_i < num_channels; channel_i++) {
             for(int roi_i = 0; roi_i < num_rois; roi_i++) {
+				float* roi_start = roi_batch_start + (roi_i * 4);
+
                 // calculate starting pixel of roi
-                int roi_flatten_index = img_i * roi_i * 4;
-                int roi_y = feature_h * rois[roi_flatten_index+0];
-                int roi_x = feature_w * rois[roi_flatten_index+1];
+                int roi_y = feature_h * *(roi_start+0);
+                int roi_x = feature_w * *(roi_start+1);
+				int roi_h = *(roi_start+2);
+				int roi_w = *(roi_start+3);
 
                 // calculate kernel size
-                int kernel_h = rois[roi_flatten_index+2];
-                int kernel_w = rois[roi_flatten_index+3];
-            }
+                int kernel_h = roi_h / output_h;
+                int kernel_w = roi_w / output_w;
+
+				for(int output_x = 0; output_x < output_w; output_x++) {
+					for(int output_y = 0; output_y < output_y; output_y++) {
+						float max_value = -1;
+
+						for(int x = 0; x < kernel_w; x++) {
+							for(int y = 0; y < kernel_h; y++) {
+								float* i = feature_batch_start +
+											(y * feature_w * num_channels) +
+											(x * num_channels) +
+											channel_i;
+								max_value = *(i) > max_value ? *(i) : max_value;
+							}
+						}
+
+						output[img_i * output_h * output_w * num_channels +
+								output_y * output_w * num_channels + 
+								output_x * num_channels + channel_i] = max_value;
+					}
+				}
+			}
         }
     }
 }
